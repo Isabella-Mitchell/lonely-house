fe98ae2ddfd8626d0cbfea977d706ea929ae3a0e
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-10-25 07:39:48 +0000

    Added listing views, urls and template

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Listing
 
-# Create your views here.
+
+def all_listings(request):
+    """A view to return all listings including sorting and search queries"""
+
+    listings = Listing.objects.all()
+
+    context = {
+        'listings': listings,
+    }
+
+    return render(request, 'listings/listings.html', context)
