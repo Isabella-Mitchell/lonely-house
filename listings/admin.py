f1340e2cf5e59f1dcfcffc48fe53e23db54d6b36
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-10-25 07:28:25 +0000

    Customised Admin view for categories and listings

--- listings/admin.py
+++ listings/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from .models import Listing, Category
 
-# Register your models here.
-admin.site.register(Listing)
-admin.site.register(Category)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'country',
+        'no_sleeps',
+        'image',
+    )
+
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Category, CategoryAdmin)
--- listings/models.py
+++ listings/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
