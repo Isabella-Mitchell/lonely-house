4b3d655363f1d4d3b2d1ad88876139ea5277896c
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-10-25 09:18:49 +0000

    Add basic search functionality

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Listing
 
 
@@ -6,9 +8,23 @@ def all_listings(request):
     """A view to return all listings including sorting and search queries"""
 
     listings = Listing.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('listings'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            listings = listings.filter(queries)
 
     context = {
         'listings': listings,
+        'search_term': query,
     }
 
     return render(request, 'listings/listings.html', context)
