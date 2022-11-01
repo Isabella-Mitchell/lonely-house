5ae7497195994b209d17632d4273d61d9407e476
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-11-01 19:03:01 +0000

    Attempt to add multi-select filters. Removed code that did not work

--- listings/views.py
+++ listings/views.py
@@ -1,55 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+# from django.http import QueryDict
 from .models import Listing, Category, Image, Facility
-
-
-def is_valid_queryparam(param):
-    return param != '' and param is not None
-
-
-# def filter(request):
-#     qs = Listing.objects.all()
-#     # filter_category = request.GET.get('category')
-#     filter_sleeps = request.GET.get('sleeps')
-#     # filter_facilities = request.GET.get('facilities')
-
-#     # if is_valid_queryparam(filter_category):
-#     #     qs = qs.filter(category=filter_category)
-
-#     if is_valid_queryparam(filter_sleeps):
-#         qs = qs.filter(no_sleeps=filter_sleeps)
-
-#     # if is_valid_queryparam(filter_facilities):
-#     #     qs = qs.filter(facilities=filter_facilities)
-
-#     return qs
-
-
-# def all_listings(request):
-
-#     listings = Listing.objects.all()
-#     query = None
-#     qs = filter(request)
-
-#     if request.GET:
-#         if 'q' in request.GET:
-#             query = request.GET['q']
-#             if not query:
-#                 messages.error(
-#                     request, "You didn't enter any search criteria!")
-#                 return redirect(reverse('listings'))
-
-#             queries = Q(name__icontains=query) | Q(
-#                 description__icontains=query)
-#             listings = listings.filter(queries)
-
-#     context = {
-#         'listings': listings,
-#         'search_term': query,
-#         'queryset': qs,
-#     }
-#     return render(request, "listings/listings.html", context)
 
 
 def all_listings(request):
@@ -66,7 +19,10 @@ def all_listings(request):
     if request.GET:
 
         if 'category' in request.GET:
+            category_list = request.GET['category']
+            print(category_list)
             categories_query_name = request.GET['category']
+            print(categories_query_name)
             categories_query = request.GET['category'].split(',')
             listings = listings.filter(category__name__in=categories_query)
             categories_query = Category.objects.filter(name__in=categories_query)
@@ -99,7 +55,7 @@ def all_listings(request):
         'categories_query_name': categories_query_name,
         'sleeps_query': sleeps_query,
         'facilities_query': facilities_query,
-        'facilities_query_name':facilities_query_name,
+        'facilities_query_name': facilities_query_name,
     }
 
     return render(request, 'listings/listings.html', context)
