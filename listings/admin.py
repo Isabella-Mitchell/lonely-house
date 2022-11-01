8f0a572fe43000864df209f9dd9a8ccf9ce9ea17
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-11-01 12:58:44 +0000

    Updated Facility as many to many relationship and updated admin

--- listings/admin.py
+++ listings/admin.py
@@ -1,18 +1,14 @@
 from django.contrib import admin
-from .models import Listing, Category, Image, Facilities
+from .models import Listing, Category, Image, Facility
 
 
 class ImageInline(admin.TabularInline):
     model = Image
 
 
-class FacilitiesInline(admin.TabularInline):
-    model = Facilities
-
-
 class ListingAdmin(admin.ModelAdmin):
 
-    inlines = (ImageInline, FacilitiesInline, )
+    inlines = (ImageInline, )
 
     list_display = (
         'name',
@@ -32,5 +28,13 @@ class CategoryAdmin(admin.ModelAdmin):
     )
 
 
+class FacilityAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+
 admin.site.register(Listing, ListingAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Facility, FacilityAdmin)
--- listings/models.py
+++ listings/models.py
@@ -16,6 +16,20 @@ class Category(models.Model):
         return self.friendly_name
 
 
+class Facility(models.Model):
+    class Meta:
+        verbose_name_plural = 'Facilities'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Listing(models.Model):
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
@@ -33,23 +47,10 @@ class Listing(models.Model):
     featured = models.BooleanField(default=False)
     lead_image = models.ImageField(null=True, blank=True)
     lead_image_url = models.URLField(max_length=1024, null=True, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
 
     def __str__(self):
         return self.name
-
-
-class Facility(models.Model):
-    class Meta:
-        verbose_name_plural = 'Facilities'
-
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-    def get_friendly_name(self):
-        return self.friendly_name
 
 
 class Image(models.Model):
