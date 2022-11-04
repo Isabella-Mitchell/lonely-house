5ad1dda61050209619c22b30a2d7b2d51c487244
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-11-04 15:28:10 +0000

    Made cart app, added urls and template

--- lonely_house/urls.py
+++ lonely_house/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('listings/', include('listings.urls')),
+    path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
