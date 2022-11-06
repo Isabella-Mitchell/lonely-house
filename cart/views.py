c816150770461da57f627f20e1f0e26f6b6d5771
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-11-06 16:16:42 +0000

    Add Toasts

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
+from django.contrib import messages
+from listings.models import Listing
 
 
 def view_cart(request):
@@ -10,6 +12,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """Add the listing booking details to the cart"""
 
+    listing = get_object_or_404(Listing, pk=item_id)
     start_date = request.POST.get('startDate')
     end_date = request.POST.get('endDate')
     no_nights = int(request.POST.get('selected-no-nights-input'))
@@ -17,18 +20,12 @@ def add_to_cart(request, item_id):
     cart = request.session.get('cart', {})
 
     # walkthrough has a if else statement here for quantity selector
-    if item_id in list(cart.keys()):
-        cart[item_id] = {
-            'no_nights': no_nights,
-            'start_date': start_date,
-            'end_date': end_date,
-        }
-    else:
-        cart[item_id] = {
-            'no_nights': no_nights,
-            'start_date': start_date,
-            'end_date': end_date,
-        }
+    cart[item_id] = {
+        'no_nights': no_nights,
+        'start_date': start_date,
+        'end_date': end_date,
+    }
+    messages.success(request, f'Added {listing.name} to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
@@ -37,12 +34,15 @@ def add_to_cart(request, item_id):
 def remove_from_cart(request, item_id):
     """Removes the listing booking details from the cart"""
 
+    listing = get_object_or_404(Listing, pk=item_id)
     cart = request.session.get('cart', {})
 
     try:
         cart.pop(item_id)
 
         request.session['cart'] = cart
+        messages.success(request, f'Removed {listing.name} from your cart')
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Error removing item {e}')
         return HttpResponse(status=500)
