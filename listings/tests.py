47e0c95795b4dfb63bc69253b4389b7011b411dc
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-11-07 21:54:27 +0000

    Add TestModels to Listing app

--- listings/tests.py
+++ listings/tests.py
@@ -10,14 +10,32 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, 'listings/listings.html')
 
     def test_get_listing_detail(self):
-        item = Listing.objects.create(
+        listing = Listing.objects.create(
             name='Test Get Listing Details', price='20.00', no_sleeps="2")
-        response = self.client.get(f'/listings/{item.id}')
+        response = self.client.get(f'/listings/{listing.id}')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'listings/listing_detail.html')
 
     # test search & filters
 
-    # test context?
+    # test context in views?
+
 
 # test form (for filters)
+
+
+class TestModels(TestCase):
+
+    def test_featured_defaults_to_false(self):
+        listing = Listing.objects.create(
+            name='Test Get Listing Details', price='20.00', no_sleeps="2")
+        self.assertFalse(listing.featured)
+
+    def test_listing_string_method_returns_name(self):
+        listing = Listing.objects.create(
+            name='Test Get Listing Details', price='20.00', no_sleeps="2")
+        self.assertEqual(str(listing), 'Test Get Listing Details')
+
+    # Add further tests for other models
+
+
