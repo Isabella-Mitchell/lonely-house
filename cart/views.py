28c2740603e6506439dde8bfc308d7bd61139658
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-11-09 12:29:29 +0000

    Customize checkout page to reflect multiple nights as line items

--- cart/views.py
+++ cart/views.py
@@ -13,7 +13,6 @@ def dates_string_to_list(str):
     selected_dates_list = str.split(',')
     # remove check out date from list so not included in price
     del selected_dates_list[-1]
-    print(selected_dates_list)
     return selected_dates_list
 
 
