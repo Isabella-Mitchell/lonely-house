9794bef0b7d21bcfc9555ba15ba42d0d701b127b
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-11-09 09:36:33 +0000

    Create Checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,69 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from listings.models import Listing
+
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    date = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)    
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    # could add below if calulcated from no nights of line orders.
+    # no_nights = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Update total each time a line item is added,
+        """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    listing = models.ForeignKey(Listing, null=False, blank=False, on_delete=models.CASCADE)
+    type = models.CharField(max_length=50, null=False, blank=False, default="Reservation")  # Reservation
+    date = models.CharField(max_length=50, null=True, blank=True)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.listing.price
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'Listing {self.listing.name} on order {self.order.order_number}'
