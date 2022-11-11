91482d64b94ece008ad512983cb3a125c4f8fe96
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-11-11 15:27:01 +0000

    Added class methods to webhook handler

--- lonely_house/settings.py
+++ lonely_house/settings.py
@@ -178,3 +178,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
