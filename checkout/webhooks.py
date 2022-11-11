38b9fc586cd03633ec155eb9078d2c258ce36b14
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-11-11 16:08:36 +0000

    Add checkout form data caching in payment intent

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -21,9 +21,7 @@ def webhook(request):
     event = None
 
     try:
-        event = stripe.Webhook.construct_event(
-        payload, sig_header, wh_secret
-        )
+        event = stripe.Webhook.construct_event(payload, sig_header, wh_secret)
     except ValueError as e:
         # Invalid payload
         return HttpResponse(status=400)
