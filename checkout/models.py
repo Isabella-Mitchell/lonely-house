4c3a8bbba6a1c8441f58662b1b30166f5e2b0cea
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-11-20 17:12:46 +0000

    Add user profile app and set up basics

--- checkout/models.py
+++ checkout/models.py
@@ -7,10 +7,13 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from listings.models import Listing
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(
+        UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
     date = models.DateTimeField(auto_now_add=True)
     first_name = models.CharField(max_length=50, null=False, blank=False)
     last_name = models.CharField(max_length=50, null=False, blank=False)
--- lonely_house/settings.py
+++ lonely_house/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'listings',
     'cart',
     'checkout',
+    'profiles',
 
     # Other
     'crispy_forms',
