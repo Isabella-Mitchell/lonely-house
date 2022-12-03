8ae43018cfc201bf7585a4c0ccaa50052712abc6
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-12-03 16:05:27 +0000

    Added profile form, views and updated templates

--- profiles/models.py
+++ profiles/models.py
@@ -18,7 +18,7 @@ class UserProfile(models.Model):
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label='Country *', null=True, blank=True)
+    default_country = CountryField(blank_label='Country', null=True, blank=True)
 
     def __str__(self):
         return self.user.username
---
+++ profiles/static/profiles/js/countryfield.js
@@ -0,0 +1,12 @@
+let countrySelected = $('#id_default_country').val();
+if(!countrySelected) {
+    $('#id_default_country').css('color', '#495057');
+};
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', '#495057');
+    } else {
+        $(this).css('color', '#000');
+    }
+});
