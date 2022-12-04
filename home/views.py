aab9703f8c289dfde449495eb44155ef044e35d1
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-12-04 10:49:26 +0000

    Add featured reviews to homepage

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 import datetime
 from datetime import date, timedelta
 from listings.models import Listing, Category
-from listings.views import listing_detail
+from reviews.models import Review
 
 
 def get_featured(model):
@@ -21,10 +21,12 @@ def index(request):
 
     featured_listings = get_featured(Listing)
     featured_categories = get_featured(Category)
+    featured_reviews = get_featured(Review)
 
     context = {
         'featured_listings': featured_listings,
         'featured_categories': featured_categories,
+        'featured_reviews': featured_reviews,
     }
 
     return render(request, 'home/index.html', context)
