0afd1e6419989fb2a687f6ceba818b2dee840d0c
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-12-10 15:06:22 +0000

    Update debug settings

--- lonely_house/settings.py
+++ lonely_house/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['lonely-house.herokuapp.com', 'localhost']
 
