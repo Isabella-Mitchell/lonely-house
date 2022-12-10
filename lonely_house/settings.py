145e8ae4ef1185bbaf8d2106c1e82e7736ab79f8
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-12-10 16:31:20 +0000

    Added cache control

--- lonely_house/settings.py
+++ lonely_house/settings.py
@@ -179,6 +179,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000'
+    }
+
+    # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'lonely-house'
     AW_S3_REGION_NAME = 'EU (London) eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
