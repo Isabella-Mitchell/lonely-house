26dd4fa18a9e387872922d7ff0f20295036df799
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-12-17 09:45:22 +0000

    Update aws parameter

--- lonely_house/settings.py
+++ lonely_house/settings.py
@@ -180,7 +180,7 @@ if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000'
+        'CacheControl': 'max-age=94608000',
     }
 
     # Bucket Config
